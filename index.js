24f61020e7fa04ceaf2f23de6cbd9a754e558b60
Lucia Gaudioso <Lucia.G@iclgo.com>
2024-03-04 16:21:48 +0000

    Feature added: it now responds with a json file with the posted url. To add: database integration, url validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
+
 const app = express();
 
 // Basic Configuration
@@ -9,14 +11,22 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
+app.use(express.urlencoded({ extended: true }));
 app.get('/', function(req, res) {
+  console.log("dsfds");
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+
+
+app.post('/api/shorturl', (req,res) =>{
+  res.json({original_url : req.body.url , short_url : 1});
+  console.log(req.body);
 });
 
 app.listen(port, function() {
